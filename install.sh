24f122689afb569295ebfa33c799fcd4e9c8beaa
MrGreenTea <mrlordalfred@gmail.com>
2016-09-07 20:47:34 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+ZSH_CUSTOM="$ZSH"/custom
+ZSH_PLUGINS="$ZSH_CUSTOM"/plugins
+
+echo "installing plugins"
+git clone https://github.com/horosgrisa/autoenv.git "$ZSH_PLUGINS"
+git clone https://github.com/b4b4r07/enhancd.git "$ZSH_PLUGINS"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_PLGUINS"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_PLUGINS"
+
+echo "installing init scripts"
+cp --symbolic-link $DIR/zsh_init_scripts/* $ZSH/custom/
