eed825636ba2f15b9fcdae356fe575d8d07a2218
jonas <jonas@jonas-netqa.de>
2016-09-15 08:47:41 +0200

    fixed install.sh

--- install.sh
+++ install.sh
@@ -5,15 +5,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-ZSH_CUSTOM="$ZSH"/custom
+ZSH_CUSTOM=~/.oh-my-zsh/custom
 ZSH_PLUGINS="$ZSH_CUSTOM"/plugins
 
 echo "installing plugins"
-git clone https://github.com/horosgrisa/autoenv.git "$ZSH_PLUGINS"
-git clone https://github.com/b4b4r07/enhancd.git "$ZSH_PLUGINS"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_PLGUINS"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_PLUGINS"
+cd "$ZSH_PLUGINS"
+
+git clone https://github.com/horosgrisa/autoenv.git
+git clone https://github.com/b4b4r07/enhancd.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+
+cd "$DIR"
 
 echo "installing init scripts"
-cp --symbolic-link "$DIR"/zsh_init_scripts/* $ZSH/custom
+cp --symbolic-link "$DIR"/zsh_init_scripts/* $ZSH_CUSTOM
 cp -fi "$DIR"/.zshrc ~/.zshrc
