bfae7c30ae8828b89a4e7210ebaab600f04faeaf
jonas <jonas@jonas-netqa.de>
2016-09-15 08:53:25 +0200

    made notebook function work correctly and made variables local

--- zsh_init_scripts/98.functions.zsh
+++ zsh_init_scripts/98.functions.zsh
@@ -1,11 +1,10 @@
-venv_name=testing
 function notebook {
-    if [[ $VIRTUAL_ENV != $WORKON_HOME"/testing" ]]
+    local venv_name=testing
+    local activated_venv=0
+    if [[ $VIRTUAL_ENV != $WORKON_HOME/$venv_name ]]
     then
-        activated_venv = 1
-        workon testing
-    else
-        activated_venv = 0
+        activated_venv=1
+        workon $venv_name
     fi
     jupyter notebook
     if [[ $activated_venv -eq 1 ]]
