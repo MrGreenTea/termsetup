de575e0ddf4d9767738c4e24fe407c269acb121b
jonas <jonas@jonas-netqa.de>
2016-09-22 13:03:47 +0200

    added python function

--- zsh_init_scripts/98.functions.zsh
+++ zsh_init_scripts/98.functions.zsh
@@ -12,3 +12,30 @@ function notebook {
         deactivate
     fi
 }
+
+_origpython=$(which python)
+
+function python {
+    if [[ $# -eq 0 ]]
+    then
+        local venv_name=testing
+        local activated_venv=0
+        if [[ $VIRTUAL_ENV != $WORKON_HOME/$venv_name ]]
+        then
+            echo "activated $venv_name environment"
+            activated_venv=1
+            workon $venv_name
+        fi
+
+        ipython
+
+        if [[ $activated_venv -eq 1 ]]
+        then
+            echo "deactivated $venv_name environment"
+            deactivate
+        fi    
+    else
+        $_origpython "$@"
+    fi
+}
+
