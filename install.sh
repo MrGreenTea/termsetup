9495b47bd9c9e6f7a4de45b2e7914a9d84a0a610
jonas <jonas@jonas-netqa.de>
2016-09-30 10:40:08 +0200

    maded install scripts separate in install_scripts dir

--- install.sh
+++ install.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo apt install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+for install_script in $DIR/install_scripts/*; do
+    source $install_script
+done
 
-ZSH_CUSTOM=~/.oh-my-zsh/custom
-ZSH_PLUGINS="$ZSH_CUSTOM"/plugins
-
-echo "installing plugins"
-cd "$ZSH_PLUGINS"
-
-git clone https://github.com/horosgrisa/autoenv.git
-git clone https://github.com/b4b4r07/enhancd.git
-git clone https://github.com/zsh-users/zsh-autosuggestions.git
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-
-cd "$DIR"
-
-echo "installing init scripts"
-cp --symbolic-link "$DIR"/zsh_init_scripts/* $ZSH_CUSTOM
-cp -fi "$DIR"/.zshrc ~/.zshrc
+unset DIR
