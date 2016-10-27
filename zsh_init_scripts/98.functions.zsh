23acecb07a8c41de1561d5ed0322c732bc396c23
jonas <jonas@jonas-netqa.de>
2016-10-27 11:42:26 +0200

    cool new stuff like pyc and gco

--- zsh_init_scripts/98.functions.zsh
+++ zsh_init_scripts/98.functions.zsh
@@ -1,15 +1,34 @@
 function notebook {
     local venv_name=testing
     local activated_venv=0
-    if [[ $VIRTUAL_ENV != $WORKON_HOME/$venv_name ]]
-    then
+    if [[ $VIRTUAL_ENV != $WORKON_HOME/$venv_name ]]; then
         activated_venv=1
         workon $venv_name
     fi
     jupyter notebook
-    if [[ $activated_venv -eq 1 ]]
-    then
+    if [[ $activated_venv -eq 1 ]]; then
         deactivate
     fi
 }
 
+function pyc {
+    local code
+    local modules
+    if [[ $1 == "-c" ]]; then
+        code=$2
+        modules=$3
+    else
+        code="from functools import partial; print=partial(print, sep='\n'); print($1)"
+        modules=$2
+    fi
+
+    if [[ -n $modules ]]; then
+        modules=$(python3 -c "print(*('from {} import *;'.format(m.strip()) for m in '$modules'.split(',')), end='')")
+    fi
+
+    /usr/bin/env python3.5 -c "$modules""from sys import *; $code"
+}
+
+function gco {
+    git status 1>/dev/null && git branch -a | cut -c 3- | sed 's|remotes/.*/||g' | pyc '*OrderedDict.fromkeys(stdin).keys(), sep="", end=""' collections | fzy --query=$1 | xargs git checkout
+}
--- zsh_init_scripts/99.aliases.zsh
+++ zsh_init_scripts/99.aliases.zsh
@@ -3,3 +3,7 @@ alias ipy=ipython
 alias dir='dir --color=auto'
 alias lf='ls -F -o --color=always | grep ^- --color=never'
 alias ff='find . | fzy'
+alias python=python3
+
+
+unalias gco
