284d4c6183961fecbd50a530a78e8d9d495e2c47
MrGreenTea <mrlordalfred@gmail.com>
2018-04-17 10:32:58 +0200

    added elementary tweaks to repoapps

--- install_scripts/10.repoapps.sh
+++ install_scripts/10.repoapps.sh
@@ -1,5 +1,5 @@
 # add repos
-repos=()
+repos=(ppa:philip.scott/elementary-tweaks)
 for repo in $repos; do
     sudo add-apt-repository "$repo"
 done
@@ -7,7 +7,7 @@ done
 sudo apt update
 
 # install
-install_apps=()
+install_apps=(elementary-tweaks)
 
 sudo apt install $install_apps
 
