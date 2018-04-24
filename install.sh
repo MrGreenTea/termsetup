d5410486ce14895d45411ac2612b2d9a0702973f
MrGreenTea <mrlordalfred@gmail.com>
2018-04-24 11:58:54 +0200

    from sh to bash in install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 for install_script in $DIR/install_scripts/*; do
-    sh $install_script || echo "$install_script didn't work"
+    bash $install_script || echo "$install_script didn't work"
 done
 
 unset DIR
