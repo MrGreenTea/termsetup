d551fbc67b4f5b1bd986e2595005de95d38eecf0
MrGreenTea <mrlordalfred@gmail.com>
2018-07-25 13:29:08 +0200

    added pipsi installation

--- install_scripts/40.python.sh
+++ install_scripts/40.python.sh
@@ -1,3 +1,4 @@
 sudo apt install libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev
 sudo apt install libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python
