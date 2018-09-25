90a35fb2f01cc2b0e9ce3df701e41acf73b53a46
Jonas Bulik <mrlordalfred@gmail.com>
2018-09-25 09:03:47 +0200

    added juno install for albert

--- install_scripts/50.albert.sh
+++ install_scripts/50.albert.sh
@@ -1,6 +1,13 @@
-lsb_release -cs | grep "loki" > /dev/null
-if [ $? -ne 0 ]; then echo "Installing albert only works for loki"; exit 1; fi
+release=$(lsb_release -cs)
+case "$release" in
+"loki")
+    sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list";;
+"juno")
+    sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list";;
+*)
+    echo "Installing albert only works for loki" && exit 1;;
+esac
+
 curl -L "https://build.opensuse.org/projects/home:manuelschneid3r/public_key" | sudo apt-key add -
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
 sudo apt update
 sudo apt install albert
