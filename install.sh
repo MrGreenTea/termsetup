2427c213f2419696702f578ef55226008cf373c5
Jonas Bulik <mrlordalfred@gmail.com>
2018-09-25 09:02:39 +0200

    fail earlier in install scripts

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -e
 
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 for install_script in $DIR/install_scripts/*; do
-    bash $install_script || echo "$install_script didn't work"
+    bash -e $install_script || >&2 echo "$install_script didn't work"
 done
 
 unset DIR
