44e87bb96d6b5946660d741406bc92297132f46a
Jonas Bulik <mrlordalfred@gmail.com>
2018-09-25 09:03:30 +0200

    fixed pipsi install

--- install_scripts/41.pipsi.sh
+++ install_scripts/41.pipsi.sh
@@ -1,3 +1,10 @@
+export PATH="~/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+# curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python
+curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python3 - --src 'git+https://github.com/mitsuhiko/pipsi.git#egg=pipsi'
+
 pipsi install poetry
-pipsi install Pygemntize
+pipsi install Pygments
 pipsi install pre-commit
