f3e173a676dd220c7b4999a784b99c488ae11249
Jonas Bulik <mrlordalfred@gmail.com>
2018-09-25 09:04:08 +0200

    theme install now with more info and unzipping

--- install_scripts/90.theme.sh
+++ install_scripts/90.theme.sh
@@ -4,16 +4,17 @@ icons_url="https://wefunkster.deviantart.com/art/MY-Remix-04-icon-theme-for-elem
 font_url="https://github.com/powerline/fonts/raw/master/SourceCodePro/Source%20Code%20Pro%20for%20Powerline.otf"
 font_file="/usr/share/fonts/type1/source-code-pro-powerline/source-code-pro-powerline.otf"
 
+echo "Download the zip files from the urls that open and then enter the path where the zip file is saved"
 
 xdg-open "$theme_url" 1>%2 2>/dev/null
 echo -n "Enter theme path: "
 read theme_file
-# sudo unzip "$theme_file" -d /usr/share/themes/
+sudo unzip "$theme_file" -d /usr/share/themes/
 
 xdg-open "$icons_url" 1>%2 2>/dev/null
 echo -n "Enter icons path: "
 read icons_file
-# sudo unzip "$icons_file" -d /usr/share/icons/
+sudo unzip "$icons_file" -d /usr/share/icons/
 
 
 sudo mkdir "$(dirname "$font_file")"
