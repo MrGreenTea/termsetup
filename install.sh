d01231888d8ad753e66e941f401caa448a1dad73
Jonas Bulik <jonas@bulik.dev>
2019-05-10 22:35:08 +0200

    fix install bash options

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -oe nounset
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 for install_script in $DIR/install_scripts/*; do
-    bash -uoe $install_script || >&2 echo "$install_script didn't work"
+    bash -ue $install_script || >&2 echo "$install_script didn't work"
 done
 
 unset DIR
