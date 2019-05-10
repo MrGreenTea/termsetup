a75c6e8df56a3ff8e52c1e5f7839065074721a8b
Jonas Bulik <mrlordalfred@gmail.com>
2019-05-10 22:24:32 +0200

    set nounset too in install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -oe nounset
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 for install_script in $DIR/install_scripts/*; do
-    bash -e $install_script || >&2 echo "$install_script didn't work"
+    bash -uoe $install_script || >&2 echo "$install_script didn't work"
 done
 
 unset DIR
