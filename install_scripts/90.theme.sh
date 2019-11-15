817f0e8ce38d72e24fa2580a6aa2a638e68cc0c8
Jonas Bulik <jonas@bulik.dev>
2019-11-15 11:33:00 +0100

    change mono font to SF Mono
    Signed-off-by: Jonas Bulik <jonas@bulik.dev>

--- install_scripts/90.theme.sh
+++ install_scripts/90.theme.sh
@@ -1,10 +1,11 @@
-font_url="https://github.com/powerline/fonts/raw/master/SourceCodePro/Source%20Code%20Pro%20for%20Powerline.otf"
-font_file="/usr/share/fonts/type1/source-code-pro-powerline/source-code-pro-powerline.otf"
+font_dir="/usr/share/fonts/type1"
+font_file="$font_dir/sf-mono.zip"
+font_url="https://github.com/Twixes/SF-Mono-Powerline/archive/master.zip"
 
-sudo mkdir "$(dirname "$font_file")"
 sudo wget "$font_url" --output-document="$font_file"
+sudo unzip "$font_file" -d "$font_dir"
 
-gsettings set org.gnome.desktop.interface monospace-font-name 'Source Code Pro for Powerline 10'
+gsettings set org.gnome.desktop.interface monospace-font-name 'SF Mono Powerline 12'
 echo "Font installed."
 
 unset font_file font_url
