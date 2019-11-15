3d151a8ee2fdcc5e3aa870d8da8358489578902f
Jonas Bulik <jonas@bulik.dev>
2019-11-15 11:02:24 +0100

    install powerline10k
    Signed-off-by: Jonas Bulik <jonas@bulik.dev>

--- install_scripts/01.ohmyzsh.sh
+++ install_scripts/01.ohmyzsh.sh
@@ -12,6 +12,7 @@ git clone https://github.com/horosgrisa/autoenv.git
 git clone https://github.com/b4b4r07/enhancd.git
 git clone https://github.com/zsh-users/zsh-autosuggestions.git
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
 
 cd "$DIR"
 
