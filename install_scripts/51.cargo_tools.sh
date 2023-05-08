82ffe3f360a612caea7a0916fa29a0dc2be04ec1
Jonas Bulik <jonas@bulik.dev>
2023-05-08 17:53:10 +0200

    upgrade & cleanup

--- install_scripts/51.cargo_tools.sh
+++ install_scripts/51.cargo_tools.sh
@@ -1,4 +1,5 @@
 export PATH="$HOME/.cargo/bin":$PATH
+cargo install cargo-binstall
 
-install_apps="bat exa zoxide bandwhich ytop fd-find du-dust starship procs sd git-delta fnm"
-cargo install $install_apps
+install_apps="bat exa zoxide bandwhich ytop fd-find du-dust starship procs sd git-delta rtx-cli"
+cargo bininstall $install_apps
