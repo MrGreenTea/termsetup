1361c63794c19b1b188ba512ca42bdccabd4ee4e
Jonas Bulik <jonas@bulik.dev>
2023-09-19 09:38:09 +0200

    Make gcm switch to main also

--- fish_config/functions/gcm.fish
+++ fish_config/functions/gcm.fish
@@ -1,4 +1,8 @@
 # Defined in - @ line 1
-function gcm --wraps='git checkout master' --description 'alias gcm=git checkout master'
-  git switch master $argv;
+function gcm --wraps='git checkout main' --description 'alias gcm=git checkout main'
+  for branch in main master;
+    if git switch $branch $argv
+      return 0
+    end
+  end
 end
