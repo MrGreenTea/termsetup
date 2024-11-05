2e0172b26d108f6fdeedcd3074d973ee4f0caa67
Jonas Bulik <jonas@bulik.dev>
2024-11-05 10:49:13 +0100

    fix gcm abbreviation

--- fish_config/conf.d/abbreviations.fish
+++ fish_config/conf.d/abbreviations.fish
@@ -5,5 +5,5 @@ abbr --add copy "xclip -selection clipboard"
 abbr --add paste "xclip -selection clipboard -out"
 abbr --add grep rg
 abbr --add cat bat
-abbr --add gbm "git switch main"
+abbr --add gcm "git switch main"
 abbr --add gbb "git bb"
