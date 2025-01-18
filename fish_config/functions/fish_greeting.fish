2462a6b3f6d1516dca0dfb7416560dc631eca4d7
Jonas Bulik <jonas@bulik.dev>
2025-01-18 08:41:25 +0100

    Cleaner fish greeting code

--- fish_config/functions/fish_greeting.fish
+++ fish_config/functions/fish_greeting.fish
@@ -1,3 +1,16 @@
 function fish_greeting
-  string join " " "I'm sorry, but" (shuf -n 1 ~/.config/fish/functions/excuses.txt | sed -e 's/^./\L&\E/')
+    # Set the path to your excuses.txt file
+    set excuse_file ~/.config/fish/functions/excuses.txt
+    
+    # Check if the excuses.txt file exists
+    if test -f $excuse_file
+        # Get a random line from the file using `shuf`
+        set random_excuse (shuf -n 1 $excuse_file)
+        
+        # Display the excuse with the desired message format
+        echo "I'm sorry, but $random_excuse"
+    else
+        # Fallback message if excuses.txt is missing
+        echo "I'm sorry, but I can't think of any excuses right now."
+    end
 end
