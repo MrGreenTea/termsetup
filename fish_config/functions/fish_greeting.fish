89d4e6feac0476b603f53eabeb3bf8316ff5fc8f
Jonas Bulik <jonas@bulik.dev>
2025-01-27 09:24:55 +0100

    Better excuses

--- fish_config/functions/fish_greeting.fish
+++ fish_config/functions/fish_greeting.fish
@@ -1,14 +1,17 @@
 function fish_greeting
     # Set the path to your excuses.txt file
     set excuse_file ~/.config/fish/functions/excuses.txt
+    set excuse_stem_file ~/.config/fish/functions/excuse_stems.txt
 
     # Check if the excuses.txt file exists
     if test -f $excuse_file
+        # random intro to the excuse
+        set intro (shuf -n 1 $excuse_stem_file)
         # Get a random line from the file using `shuf`
         set random_excuse (shuf -n 1 $excuse_file)
 
         # Display the excuse with the desired message format
-        echo "I'm sorry, but $random_excuse"
+        echo "$intro $random_excuse"
     else
         # Fallback message if excuses.txt is missing
         echo "I'm sorry, but I can't think of any excuses right now."
