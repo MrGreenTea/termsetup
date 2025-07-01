a9a46001442931cedddd6d6c928f4a4d425bc023
Jonas Bulik <jonas@bulik.dev>
2025-07-01 20:33:22 +0200

    More cool rust tools

--- install_scripts/51.cargo_tools.sh
+++ install_scripts/51.cargo_tools.sh
@@ -4,5 +4,5 @@ export PATH="$HOME/.cargo/bin":$PATH
 sudo dnf groupinstall "Development Tools"
 cargo install cargo-binstall
 
-install_apps="bat exa zoxide bandwhich ytop fd-find du-dust starship procs sd git-delta rtx-cli ripgrep atuin bottom"
+install_apps="bat exa zoxide bandwhich ytop fd-find du-dust starship procs sd git-delta rtx-cli ripgrep atuin bottom hyperfine nu broot tokei"
 cargo binstall $install_apps
