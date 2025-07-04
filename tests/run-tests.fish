3ce568a8bd095b9410d0c2bfdf11625aa5149491
Jonas Bulik <jonas@bulik.dev>
2025-07-04 18:54:33 +0200

    Add tests

--- tests/run-tests.fish
+++ tests/run-tests.fish
@@ -1,8 +1,13 @@
 #!/usr/bin/env fish
 
 # Run all Lua test files in the current directory
+echo "=== Running Lua Tests ==="
 for test in *.lua
     echo "Running $test..."
     lua $test
     echo
 end
+
+# Run Python tests
+echo "=== Running Python Tests ==="
+./test_conventional_commit_hook.py
