2d48d692da94df9e0db8d1b66ba0a870b42bb6c6
Jonas Bulik <jonas@bulik.dev>
2025-07-21 07:59:23 +0200

    fix(wezterm): add window bar

--- configs/wezterm/appearance.lua
+++ configs/wezterm/appearance.lua
@@ -6,6 +6,7 @@ function module.apply_to_config(config)
 	config.color_scheme = "Catppuccin Mocha"
 	config.font = wezterm.font("JetBrains Mono")
 	config.use_fancy_tab_bar = false
+	config.window_decorations = "RESIZE"
 end
 
 return module
