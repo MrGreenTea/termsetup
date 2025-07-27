d8130cdba12267b7719ed15bfbcaacf9c0cb1d91
Jonas Bulik <jonas@bulik.dev>
2025-07-27 18:22:55 +0200

    configure jujutsu
    try to disable all interactivity in claude code.
    It breaks

--- install_scripts/51.cargo_tools.sh
+++ install_scripts/51.cargo_tools.sh
@@ -6,3 +6,7 @@ cargo install cargo-binstall
 
 install_apps="bat exa zoxide bandwhich ytop fd-find du-dust starship procs sd git-delta git-absorb rtx-cli ripgrep atuin bottom hyperfine nu broot tokei jj-cli"
 cargo binstall $install_apps
+
+# Create jj config directory and symlink
+mkdir -p ~/.config/jj
+ln -sf "$(pwd)/configs/jj/config.toml" ~/.config/jj/config.toml
